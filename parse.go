cd47bc715b092077194810cd1578b240261dfd0b
sterling <sterlinghoughton@gmail.com>
2018-03-06 09:44:44 -0800

    create dfs function, recusively parse through html and print out nodes

--- parse.go
+++ parse.go
@@ -1,7 +1,10 @@
 package link
 
 import (
+	"fmt"
 	"io"
+
+	"golang.org/x/net/html"
 )
 
 // Link represents a link (<a href="...">) in an HTML document.
@@ -13,5 +16,17 @@ type Link struct {
 // Parse will take in an HTML document and return a slice of links
 // parsed from it
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	fmt.Println(padding, n.Data)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
